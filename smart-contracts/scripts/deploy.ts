3e667a33c70473dd8e15335daf15b2b4b3ef18a6
ccddan <ccdd.dev@gmail.com>
2022-05-19 02:01:47 -0500

    sc:feat: deploy WavePortal contract
    - remove Greeter contract deployment

--- smart-contracts/scripts/deploy.ts
+++ smart-contracts/scripts/deploy.ts
@@ -1,29 +1,17 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const [owner, acc1] = await ethers.getSigners();
+  const contractName = "WavePortal";
+  const WavePortal = await ethers.getContractFactory(contractName);
+  const wavePortal = await WavePortal.deploy();
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  await wavePortal.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log(`${contractName} deployed to:`, wavePortal.address);
+  console.log(`${contractName} contract owner:`, owner.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
