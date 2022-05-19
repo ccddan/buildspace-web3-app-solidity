d4f8aebb2756a8c4f6f696369f6072dec6566836
ccddan <ccdd.dev@gmail.com>
2022-05-19 18:04:14 -0500

    wc:fix: scape single quote

--- web-client/pages/index.tsx
+++ web-client/pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
           <div className="header">👋 Hey there!</div>
 
           <div className="bio">
-            I am Anonymous and I am learning Web 3 and Blockchain so that's
+            I am Anonymous and I am learning Web 3 and Blockchain so that&apos;s
             pretty cool right?
             <br />
             Connect your Ethereum wallet and wave at me!
