f7be067389eb09ab0edc8503379f8141ab2d2fbc
ccddan <ccdd.dev@gmail.com>
2022-05-19 23:32:18 -0500

    sc:feat: add wave message
    - keep track of messages
    - retrieve all waves/messages

--- smart-contracts/contracts/WavePortal.sol
+++ smart-contracts/contracts/WavePortal.sol
@@ -8,13 +8,27 @@ contract WavePortal {
     uint256 private totalWaves;
     mapping(address => uint256) private wavers;
 
+    event NewWave(address indexed from, string message, uint256 timestamp);
+
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
+    }
+
+    Wave[] private waves;
+
     constructor() {
         console.log("Contract initialized");
     }
 
-    function wave() public {
+    function wave(string memory message) public {
         totalWaves += 1;
         wavers[msg.sender] += 1;
+
+        waves.push(Wave(msg.sender, message, block.timestamp));
+        emit NewWave(msg.sender, message, block.timestamp);
+
         console.log("%s has waved!", msg.sender);
     }
 
@@ -25,5 +39,9 @@ contract WavePortal {
 
     function getTotalWavesByWaver(address waver) public view returns (uint256) {
         return wavers[waver];
+    }
+
+    function getWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 }
